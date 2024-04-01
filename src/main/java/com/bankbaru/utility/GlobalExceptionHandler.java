6173e1f31f3a1b8616e2dbb81898007e139cb7d6
lalurikya <149987830+lalurikya@users.noreply.github.com>
2024-04-01 10:01:24 +0700

    update

--- src/main/java/com/bankbaru/utility/GlobalExceptionHandler.java
+++ src/main/java/com/bankbaru/utility/GlobalExceptionHandler.java
@@ -27,19 +27,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-        // Mendapatkan pesan kesalahan dari HttpMessageNotReadableException
         ResponseDTO responseDTO = new ResponseDTO(HttpStatus.BAD_REQUEST.value(), "Masukan Format inputan yang sesuai, format nomor KTP berupa angka max 16 digit, format tanggal lahir yyyy-MM-dd");
-
-        // Mengembalikan respons dengan status HTTP 400 Bad Request dan pesan kesalahan yang sesuai
         return ResponseEntity.badRequest().body(responseDTO);
     }
 
-    // Anda dapat menambahkan penanganan untuk jenis kesalahan lain di sini sesuai kebutuhan
-
-//     Contoh penanganan kesalahan umum
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleGenericException(Exception ex) {
-        ResponseDTO responseDTO = new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Terjadi kesalahan pada server.");
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseDTO);
-    }
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<Object> handleGenericException(Exception ex) {
+//        ResponseDTO responseDTO = new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Terjadi kesalahan pada server.");
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseDTO);
+//    }
 }
