d64d86de77d4ca243247ecb40ba1a7b62a25660e
lalurikya <149987830+lalurikya@users.noreply.github.com>
2024-04-01 10:37:20 +0700

    update global error handler

--- src/main/java/com/bankbaru/utility/GlobalExceptionHandler.java
+++ src/main/java/com/bankbaru/utility/GlobalExceptionHandler.java
@@ -31,9 +31,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(responseDTO);
     }
 
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<Object> handleGenericException(Exception ex) {
-//        ResponseDTO responseDTO = new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Terjadi kesalahan pada server.");
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseDTO);
-//    }
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        ResponseDTO responseDTO = new ResponseDTO(HttpStatus.NOT_FOUND.value(), "Halaman tidak ditemukan.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDTO);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGenericException(Exception ex) {
+        ResponseDTO responseDTO = new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Terjadi kesalahan pada server.");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseDTO);
+    }
 }
